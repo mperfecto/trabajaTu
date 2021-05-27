64f4abe0b81a53a6e0b49a34b71971cb7ba48c53
mperfecto <mperfectoarjona@gmail.com>
2021-05-28 01:52:33 +0200

    funcionalidad guardar jugadores

--- src/main/java/com/ssii/project/controllers/InicioController.java
+++ src/main/java/com/ssii/project/controllers/InicioController.java
@@ -1,18 +1,63 @@
 package com.ssii.project.controllers;
 
-import com.ssii.project.services.ContarVyC;
+import java.text.MessageFormat;
 
+import com.ssii.project.models.ProjectModel;
+import com.ssii.project.services.ContarLetrasService;
+import com.ssii.project.services.ProjectBDService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InicioController {
 
+    @Autowired
+    ContarLetrasService contarLetrasService;   
+    
+    @Autowired
+    ProjectBDService projectBDService;
+
+    @Autowired
+    ProjectModel projectModel;
+
+    @Bean
+    ProjectModel getprojectmodel(){
+		return new ProjectModel();
+	}
+    
     @GetMapping("/contar/{palabra}")
-    public String VocalesConsonantes(@PathVariable String palabra) {
-        String res = ContarVyC.contarLetras(palabra);
-        return res;
+    public String vocalesConsonantes(@PathVariable String palabra) {
+        return ContarLetrasService.contarLetras(palabra);
+    }
+
+    @GetMapping("/listar")
+    public String jugadores() {
+        return projectBDService.obtenerDatos().toString();
+    }
+
+    @GetMapping("/guardar")
+    public String GuardaPorQuery(@RequestParam String nombre,@RequestParam String apellido,@RequestParam int dorsal){
+
+        ProjectModel nuevoJugador = new ProjectModel();
+
+        Object params[] = {nombre, apellido, dorsal};
+
+        String nombreSt = params[0].toString();
+        String apellidoSt = params[1].toString();
+        int dorsalSt = (int)params[2];
+        
+        nuevoJugador.setNombre(nombreSt);
+        nuevoJugador.setApellido(apellidoSt);
+        nuevoJugador.setDorsal(dorsalSt);
+
+        projectBDService.guardarDatos(nuevoJugador);
+
+        return MessageFormat.format("Se han guardado lo datos del jugador {0} {1} con el dorsal {2}", params);
     }
 
 }
---
+++ src/main/java/com/ssii/project/models/ProjectModel.java
@@ -0,0 +1,79 @@
+package com.ssii.project.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "jugador")
+public class ProjectModel {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String nombre;
+ 
+    private String apellido;
+
+    private int dorsal;
+ 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date fecha; // Si no pongo fecha, la fecha será la del sistema
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Datos jugador [nombre=" + nombre + ", apellido=" + apellido + ", dorsal=" + dorsal + "]";
+    }    
+
+}
